833b90e7eea5022c8e6413e468d0c3e0677596e8
DawidB7 <podlasieauuau@gmail.com>
2025-01-31 12:39:48 +0100

    notificationBig picture

--- app/src/main/java/com/example/notification/MainActivity.java
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
@@ -12,6 +14,7 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.graphics.BitmapCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -30,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(v->sendNotification());
         Button button1 = findViewById(R.id.NotificationLong);
         button1.setOnClickListener(view -> sendNotificationLong());
+        Button button2 = findViewById(R.id.NotificationPicture);
+        button2.setOnClickListener(view -> sendNotificationLargeIcon());
 
     }
 
@@ -76,6 +81,26 @@ public class MainActivity extends AppCompatActivity {
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setContentTitle("Nowe powiadomienie")
                 .setStyle(new NotificationCompat.BigTextStyle().bigText("fadjlkdfhlkddlkjdlkjdaslkjdlakjdlkadjalkjdaldjaslkdjaslkdjalkdjaslkdjalkjdaslkjda"))
+                .setContentText("Treść powiadomienia")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, builder.build());
+    }
+
+    private void sendNotificationLargeIcon(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+                return;
+            }
+        }
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cfaniak);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Nowe powiadomienie")
+                .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap))
                 .setContentText("Treść powiadomienia")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setAutoCancel(true);
